d238a32494591669a118568edf493dbe2fd40853
Brandon Gottlob <bgottlob1@gmail.com>
2014-04-09 16:42:46 -0400

    Got the food list working

---
+++ SnackTrack/ListViewController.m
@@ -0,0 +1,89 @@
+//
+//  ListViewController.m
+//  SnackTrack
+//
+//  Created by Brandon Gottlob on 4/9/14.
+//  Copyright (c) 2014 Brandon Gottlob. All rights reserved.
+//
+
+#import "ListViewController.h"
+#import "FoodList.h"
+#import "FoodItem.h"
+#import "AppDelegate.h"
+
+@implementation ListViewController
+
+@synthesize foodTable;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    foodTable.delegate = self;
+    foodTable.dataSource = self;
+}
+
+//Called whenever a list view is about to appear
+-(void)viewWillAppear:(BOOL)animated
+{
+    [foodTable reloadData];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UITableViewCell"];
+    
+    //Get a reference to the AppDelegate object
+    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    
+    //Get a reference to the current item
+    FoodItem *item = [appDelegate.foodList.foodArray objectAtIndex:indexPath.row];
+    [cell.textLabel setText:item.name];
+    
+    [cell setBackgroundColor:[UIColor clearColor]];
+    [cell.textLabel setTextColor:[UIColor blackColor]];
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    return cell;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    //Get a reference to the AppDelegate object
+    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    
+    return [appDelegate.foodList.foodArray count];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
