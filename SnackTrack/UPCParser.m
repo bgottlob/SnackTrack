cdc974f58e3fab6ee6435b0c8a68259f0c0cbea6
nmilkosky <nmilkosky@gmail.com>
2014-04-09 14:08:16 -0400

    Create UPCParser.m

---
+++ SnackTrack/UPCParser.m
@@ -0,0 +1,31 @@
+//
+//  UPCParser.m
+//  SnackTrack
+//
+//  Created by Nathaniel Milkosky on 4/8/14.
+//  Copyright (c) 2014 Brandon Gottlob. All rights reserved.
+//
+
+#import "UPCParser.h"
+
+@implementation UPCParser
+
+    +(NSDictionary *)parseUPC:(NSString*)upcCode
+    {
+        NSLog(@"here");
+        NSString *combined = [@"http://api.upcdatabase.org/xml/e9b06e801511e9b6492ac371850bdc83/" stringByAppendingString:upcCode];
+        NSLog(@"%@", combined);
+        NSURL *xmlURL = [[NSURL alloc] initWithString:combined];
+        NSXMLParser *XMLupcparser =[[NSXMLParser alloc] initWithContentsOfURL:xmlURL];
+        UPCdelegate *parser = [[UPCdelegate alloc] initUPCdelegate];
+        [XMLupcparser setDelegate:parser];
+        BOOL success = [XMLupcparser parse];
+        if(success){
+            NSMutableDictionary *itemData = [parser item];
+            NSLog(@"succ");
+            return itemData;
+        }
+        return NULL;
+    }
+    
+@end
