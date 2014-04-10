3a738e1172768910ca02efce8910a7e3cb1b6be7
nmilkosky <nmilkosky@gmail.com>
2014-04-10 14:33:07 -0400

    Update UPCParser.m
    Added comments

--- SnackTrack/UPCParser.m
+++ SnackTrack/UPCParser.m
@@ -15,17 +15,24 @@
     //Generating a URL to feed to the API
     NSString *combined = [@"http://api.upcdatabase.org/xml/e9b06e801511e9b6492ac371850bdc83/" stringByAppendingString:upcCode];
     NSURL *xmlURL = [[NSURL alloc] initWithString:combined];
+    
+    //Start the parser with data from the specified URL
     NSXMLParser *XMLupcparser =[[NSXMLParser alloc] initWithContentsOfURL:xmlURL];
     
+    //Set up the delagate for the parser
     UPCdelegate *parser = [[UPCdelegate alloc] initUPCdelegate];
     [XMLupcparser setDelegate:parser];
     
+    //Parse the data and store if it was successful or not
     BOOL success = [XMLupcparser parse];
+    
+    //If it was successful, create the dictionary and return it
     if(success){
         NSMutableDictionary *itemData = [parser item];
         return itemData;
     }
     
+    //If it wasn't successful, return NULL;
     return NULL;
 }
 
