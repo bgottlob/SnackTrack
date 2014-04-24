2cae8c0306d5ddfee8b2fb07f4971b81fe74c647
Thomas Borgia <tborgia@c-68-39-86-134.hsd1.nj.comcast.net>
2014-04-23 22:39:46 -0400

    Added date picker view to expiration date input. Tried to make edit mode work via edit button, but breaks detail view and when the app is restarted the expiration date entry disappears.

---
+++ SnackTrack/EditViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditViewController.h
+//  SnackTrack
+//
+//  Created by Thomas Borgia on 4/21/14.
+//  Copyright (c) 2014 Brandon Gottlob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditViewController : UIViewController
+
+@end
