fbda3a348a9e1e59c94915fcc4b69fddd6263f79
Thomas Borgia <tborgia@c-68-39-86-134.hsd1.nj.comcast.net>
2014-04-27 21:30:24 -0400

    Fixed some edit view bugs

--- SnackTrack/EditViewController.m
+++ SnackTrack/EditViewController.m
@@ -17,7 +17,7 @@
 
 @implementation EditViewController
 
-@synthesize qtyField, foodName, upc, expiryDate, description, rowNo;
+@synthesize qtyField, foodName, upc, expiryDate, description, rowNo, scrollView, keyboardIsShown;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -31,9 +31,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    //Makes sure that user interface elements do not appear underneath the navigation bar in iOS 7
+    if ([UIDevice currentDevice].systemVersion.floatValue >= 7.0)
+    {
+        self.edgesForExtendedLayout = UIRectEdgeNone;
+        self.automaticallyAdjustsScrollViewInsets = NO;
+    }
+    
     //Get a reference to the AppDelegate object
     AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    
+    //504 is the height of the scroll view on iPhone 5 screens!!!!
+    [self.scrollView setContentSize:CGSizeMake(320, 504)];
+    
+    // register for keyboard notifications
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:self.view.window];
+    // register for keyboard notifications
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:self.view.window];
+    self.keyboardIsShown = NO;
     
     //Setup UIStepper for quantity value
     [self.stepper setMinimumValue:1];
@@ -42,6 +58,12 @@
     [self.stepper setStepValue:1];
     [self.stepper setMaximumValue:100];
     [self.stepper setValue:((FoodItem*)[appDelegate.foodList.foodArray objectAtIndex:rowNo]).quantity];
+    
+    self.foodName.delegate = self;
+    self.upc.delegate = self;
+    self.expiryDate.delegate = self;
+    self.description.delegate = self;
+    self.qtyField.delegate = self;
     
     //Setup UIDatePicker for expiration date input
     UIDatePicker *datePicker = [[UIDatePicker alloc]init];
@@ -61,6 +83,48 @@
     expiryDate.text = [formatter stringFromDate:((FoodItem*)[appDelegate.foodList.foodArray objectAtIndex:rowNo]).expiryDate];
     description.text = ((FoodItem*)[appDelegate.foodList.foodArray objectAtIndex:rowNo]).description;
 }
+
+- (void)keyboardWillHide:(NSNotification *)n
+{
+    CGRect viewFrame = self.scrollView.frame;
+    viewFrame.size.height = 504;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [self.scrollView setFrame:viewFrame];
+    [UIView commitAnimations];
+    keyboardIsShown = NO;
+}
+
+- (void)keyboardWillShow:(NSNotification *)n
+{
+    // This is an ivar I'm using to ensure that we do not do the frame size adjustment on the `UIScrollView` if the keyboard is already shown.  This can happen if the user, after fixing editing a `UITextField`, scrolls the resized `UIScrollView` to another `UITextField` and attempts to edit the next `UITextField`.  If we were to resize the `UIScrollView` again, it would be disastrous.  NOTE: The keyboard notification will fire even when the keyboard is already shown.
+    if (keyboardIsShown) {
+        return;
+    }
+    
+    NSDictionary* userInfo = [n userInfo];
+    
+    // get the size of the keyboard
+    CGSize keyboardSize = [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    // resize the noteView
+    CGRect viewFrame = self.scrollView.frame;
+    viewFrame.size.height -= (keyboardSize.height + 10);
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [self.scrollView setFrame:viewFrame];
+    [UIView commitAnimations];
+    keyboardIsShown = YES;
+}
+
+//This method is required to allow the scroll view to scroll for some reason
+-(void)viewDidLayoutSubviews
+{
+    [scrollView setContentSize:CGSizeMake(320, 504)];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -121,16 +185,16 @@
 }
 
 
--(BOOL)textFieldShouldReturn:(UITextField *)textField
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     if (textField == self.foodName) {
         [self.upc becomeFirstResponder];
     } else if (textField == self.upc) {
-        [self.expiryDate becomeFirstResponder];
-    } else if (textField == self.expiryDate) {
         [self.description becomeFirstResponder];
     } else if (textField == self.description) {
-        [self.description resignFirstResponder];
+        [self.expiryDate becomeFirstResponder];
+    } else if (textField == self.expiryDate) {
+        [self.expiryDate resignFirstResponder];
     }
     return YES;
 }
